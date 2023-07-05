8a6ca4bd587d6c453f61a75917b9dba927167a46
lov1s <artem.boss619@gmail.com>
2023-07-05 19:28:49 +0300

    eslint add and fix files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import {Suspense} from 'react';
-import './styles/index.scss';
-import {useTheme} from "app/providers/ThemeProvider";
-import {classNames} from "shared/lib/classNames/classNames";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
-import {Sidebar} from "widgets/Sidebar";
-
+import { Suspense } from 'react'
+import './styles/index.scss'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { AppRouter } from 'app/providers/router'
+import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
 
 const App = () => {
-    const {theme} = useTheme()
-    return (
+  const { theme } = useTheme()
+  return (
         <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback={""} >
+            <Suspense fallback={''} >
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
@@ -20,7 +19,7 @@ const App = () => {
             </Suspense>
 
         </div>
-    );
-};
+  )
+}
 
-export default App;
+export default App
