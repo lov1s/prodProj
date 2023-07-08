5ca833904793c32ef5e8cff0aabd7a5ac5153841
lov1s <artem.boss619@gmail.com>
2023-07-09 00:07:35 +0300

    404 page add loading animation and server latency

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,3 +1,12 @@
 import { lazy } from "react";
 
-export const AboutPageAsync = lazy(async () => await import("./AboutPage"));
+export const AboutPageAsync = lazy(
+    async () =>
+        await new Promise((resolve) => {
+            setTimeout(() => {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-expect-error
+                resolve(import("./AboutPage"));
+            }, 1500);
+        })
+);
