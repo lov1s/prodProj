deea163007e3e5aef2f4557560ab2b83a9e0fefa
lov1s <artem.boss619@gmail.com>
2023-07-27 18:45:06 +0300

    fix error lint

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
-import cls from "./ThemeSwitcher.module.scss";
-import { classNames } from "shared/lib/classNames/classNames";
+// import cls from "./ThemeSwitcher.module.scss";
+// import { classNames } from "shared/lib/classNames/classNames";
 import { Theme, useTheme } from "app/providers/ThemeProvider";
 import DarkIcon from "shared/assets/icons/dark-mode.svg";
 import LightIcon from "shared/assets/icons/light-mode.svg";
@@ -13,7 +13,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.ThemeSwitcher, {}, [className!])}
+            // className={classNames(cls.ThemeSwitcher, {}, [className!])}
             onClick={toggleTheme}
         >
             {theme === Theme.LIGHT ? <DarkIcon fill={"#fff"} /> : <LightIcon />}
