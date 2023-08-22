67dbeb2ee05274693eecf6d81f507328474ada09
lov1s <artem.boss619@gmail.com>
2023-08-22 10:44:38 +0300

    Add profile page

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,5 +1,5 @@
 import { StoreProvider } from "./ui/StoreProvider";
-import { createReduxStore, AppDispatch } from "./config/store";
+import { createReduxStore, type AppDispatch } from "./config/store";
 import { type StateSchema } from "./config/StateSchema";
 
-export { StoreProvider, createReduxStore, type StateSchema, AppDispatch };
+export { StoreProvider, createReduxStore, type StateSchema, type AppDispatch };
